6a55705bab393afb2a1fb047d587a63887ed7eea
trashboat <aidenthakur05@gmail.com>
2023-10-10 19:57:15 -0700

    Update moveCapt.m

--- CRABS/moveCapt.m
+++ CRABS/moveCapt.m
@@ -2,9 +2,10 @@ function  [ xCapt, yCapt, thetaCapt ] = moveCapt( cmd, x, y, theta );
 
 dTheta = pi/6;
 dStep = 50;
+
 if( cmd == "w" ) %move forward
-xCapt = x dstep*cos(theta);
-yCapt = x dstep*sin(theta);
+xCapt = x + dstep*cos(theta);
+yCapt = x + dstep*sin(theta);
 thetaCapt = theta;
 
 elseif ( cmd == "d" ) %turn right
